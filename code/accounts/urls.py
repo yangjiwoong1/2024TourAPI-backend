fdbdef6b653fe67833299b9c728f4bc01b31402a
yangjiwoong1 <wpyang123@naver.com>
2024-07-28 22:15:26 +0900

    refactor: 회원가입 API request, response 포맷 수정 및 전체 코드 리팩토링
    - CreateAPIView의 기능 활용을 위한 전체적인 코드 리팩토링
    - request, response format 변경

--- code/accounts/urls.py
+++ code/accounts/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import UserRegisterView
 
 urlpatterns = [
-    path("signup/", UserRegisterView.as_view()), # post - 회원가입
+    path("", UserRegisterView.as_view()), # post - 회원가입
 ]
