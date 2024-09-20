d70e8b8bf9e5d6c1965c41b8f7e4d23eddd2038c
yangjiwoong1 <wpyang123@naver.com>
2024-09-20 12:34:11 +0900

    refactor: base url 수정
    - base url에 /api 추가

--- code/config/urls.py
+++ code/config/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('users/', include('accounts.urls'))
+    path('api/users/', include('accounts.urls'))
 ]
