510dc2a838fc29400799a5c602f425c5c255e063
yangjiwoong1 <wpyang123@naver.com>
2024-09-22 03:05:16 +0900

    feat: 여행지 수정 api 생성
    - 여행지의 날짜, 시간을 수정할 수 있는 api 생성

--- code/planners/urls.py
+++ code/planners/urls.py
@@ -4,4 +4,5 @@ from .views import *
 urlpatterns = [
     path('', PlanView.as_view(), name='create-plan'),
     path('destinations/', DestinationView.as_view(), name='create-destination'),
+    path('destinations/<int:destinationId>/', DestinationView.as_view(), name='update-destination'),
 ]
--- code/planners/views.py
+++ code/planners/views.py
@@ -1,6 +1,7 @@
 from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from .models import Destination
 from .serializers import PlanSerializer, DestinationSerializer
 
 class PlanView(APIView):
@@ -33,6 +34,33 @@ class DestinationView(APIView):
                 'destination': serializer.data
             }
             return Response(res, status=status.HTTP_201_CREATED)
+
+        res = {
+            'success': False,
+            'status_code': status.HTTP_400_BAD_REQUEST,
+            'message': serializer.errors
+        }
+        return Response(res, status=status.HTTP_400_BAD_REQUEST)
+    
+    def patch(self, request, destinationId):
+        try:
+            destination = Destination.objects.get(pk=destinationId)
+        except Destination.DoesNotExist:
+            return Response({
+                'success': False,
+                'status_code': status.HTTP_404_NOT_FOUND,
+                'message': '일정에 해당 목적지가 존재하지 않습니다.'
+            }, status=status.HTTP_404_NOT_FOUND)
+
+        serializer = DestinationSerializer(destination, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            res = {
+                'success': True,
+                'status_code': status.HTTP_200_OK,
+                'destination': serializer.data
+            }
+            return Response(res, status=status.HTTP_200_OK)
 
         res = {
             'success': False,
