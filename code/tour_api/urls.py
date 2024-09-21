e0f7aa4c421c9ec256c88ca9bad1cb5984b8e087
yangjiwoong1 <wpyang123@naver.com>
2024-09-22 07:32:27 +0900

    feat: 관광지 조회 API 생성
    - 관광지 상세정보를 조회하는 API 생성

--- code/tour_api/urls.py
+++ code/tour_api/urls.py
@@ -3,4 +3,5 @@ from .views import *
 
 urlpatterns = [
     path('tourist-attractions/', TouristAttractionView.as_view(), name='get-attractions'),
+    path('tourist-attractions/<int:contentId>/', TouristAttractionDetailView.as_view(), name='get-attractions-detail'),
 ]
--- code/tour_api/views.py
+++ code/tour_api/views.py
@@ -10,8 +10,9 @@ class TouristAttractionView(APIView):
         limit = request.GET.get('limit', 10)
         page = request.GET.get('page', 1)
         areacode = request.GET.get('areacode', None)
+        content_type_id = request.GET.get('contentTypeId', None)
 
-        response = fetch_searchKeyword1(search, limit, page, areacode)
+        response = fetch_searchKeyword1(search, limit, page, areacode, content_type_id)
 
         try:
             response.raise_for_status()
@@ -35,16 +36,72 @@ class TouristAttractionView(APIView):
 
         filtered_items = [
             {
+                'contentid': item.get('contentid'),
+                'contenttypeid': item.get('contenttypeid'),
                 'title': item.get('title'),
-                'mapx': item.get('mapx'),
-                'mapy': item.get('mapy'),
-                'contentid': item.get('contenttypeid'),
-                'addr1': item.get('addr1'),
+                'tel': item.get('tel'),
+                'modifiedtime': item.get('modifiedtime'),
+                'firstimage2': item.get('firstimage2'),
                 'areacode': item.get('areacode'),
                 'sigungucode': item.get('sigungucode'),
-                'firstimage2': item.get('firstimage2'),
+                'addr1': item.get('addr1'),
+                'mapx': item.get('mapx'),
+                'mapy': item.get('mapy')
+            }
+            for item in items
+        ]
+
+        res = {
+            'success': True,
+            'status_code': status.HTTP_200_OK,
+            'results': filtered_items
+        }
+        return Response(res, status=status.HTTP_200_OK)
+
+class TouristAttractionDetailView(APIView):
+    def get(self, request, contentId):
+        limit = request.GET.get('limit', 10)
+        page = request.GET.get('page', 1)
+
+        response = fetch_detailCommon1(contentId, limit, page)
+
+        try:
+            response.raise_for_status()
+            data = response.json()
+            if data.get("response", {}).get("body", {}).get("items", {}):
+                items = data.get("response", {}).get("body", {}).get("items", {}).get("item", [])
+            else:
+                items = []
+        except requests.HTTPError as http_err:
+            return Response({
+                'success': False,
+                'status_code': status.HTTP_500_INTERNAL_SERVER_ERROR,
+                'message': f'HTTP 에러 발생: {http_err}'
+            }, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        except Exception as err:
+            return Response({
+                'success': False,
+                'status_code': status.HTTP_500_INTERNAL_SERVER_ERROR,
+                'message': f'에러 발생: {err}'
+            }, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+        filtered_items = [
+            {
+                'contentid': item.get('contentid'),
+                'contenttypeid': item.get('contenttypeid'),
+                'title': item.get('title'),
+                'tel': item.get('tel'),
+                'homepage': item.get('hompage'),
                 'modifiedtime': item.get('modifiedtime'),
-                'tel': item.get('tel')
+                'firstimage': item.get('firstimage'),
+                'firstimage2': item.get('firstimage2'),
+                'areacode': item.get('areacode'),
+                'sigungucode': item.get('sigungucode'),
+                'addr1': item.get('addr1'),
+                'addr2': item.get('addr2'),
+                'zipcode': item.get('zipcode'),
+                'mapx': item.get('mapx'),
+                'mapy': item.get('mapy')
             }
             for item in items
         ]
