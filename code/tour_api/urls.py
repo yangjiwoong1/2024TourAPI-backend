e8988621c530d7a1188e7c131ea794382b939cee
yangjiwoong1 <wpyang123@naver.com>
2024-09-22 21:41:32 +0900

    feat: 반복정보 조회 api 생성
    - 관광지 반복정보 조회 api를 생성

--- code/tour_api/urls.py
+++ code/tour_api/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('tourist-attractions/', TouristAttractionView.as_view(), name='get-attractions'),
     path('tourist-attractions/nearby/', LocationBasedTouristAttractionView.as_view(), name='get-attractions-nearby'),
     path('tourist-attractions/<int:contentId>/', TouristAttractionDetailCommonView.as_view(), name='get-attractions-detail-common'),
-    path('tourist-attractions/<int:contentId>/intro', TouristAttractionDetailIntroView.as_view(), name='get-attractions-detail-intro'),
+    path('tourist-attractions/<int:contentId>/intro/', TouristAttractionDetailIntroView.as_view(), name='get-attractions-detail-intro'),
+    path('tourist-attractions/<int:contentId>/info/', TouristAttractionDetailInfoView.as_view(), name='get-attractions-detail-info'),
 ]
